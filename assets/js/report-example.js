25337428358f25fa07680742704744b255049403
Maeve Murphy Quinlan <earmmu@leeds.ac.uk>
2025-05-12 10:00:21 +0100

    Adding in search functionality

---
+++ assets/js/report-example.js
@@ -0,0 +1,9 @@
+const LAYER_CONFIGS = {
+    counties: {
+      label: "Parks",
+      color: "blue",
+      type: "point",
+      file: "../data/data-processing/example-parks.geojson",
+      nameField: "name"
+    },
+};
